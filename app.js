b0f8e461ecd7a44bdb473ee6306d52f99b670ec6
Aldrin <aldrinmcaalim@gmail.com>
2021-03-08 18:22:45 -0500

    Made plan in JS file

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+================================================================================== 
+BASIC SUMMARY
+==================================================================================
+Random Number Generator Guessing Game
+
+================================================================================== 
+Steps
+==================================================================================
+1) Ask user for number range they would like to guess within.
+
+2) Ask user for first guess:
+    1) If first guess is correct, then tell user they are correct and end program
+    2) If first guess is not correct, then request the user guess again
+    3) If user wants to quit at anypoint, they should enter 'quit'
+
+3) Once the correct answer is found, display the number of attempts made.
+*/
+
